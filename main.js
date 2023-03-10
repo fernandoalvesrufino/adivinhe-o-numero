4884326e2eca5fdcf19f4c176e61e90cf6ee4e77
fernandorufino <fernandorufinoalves@gmail.com>
2023-03-10 17:23:34 -0300

    refatorando codigo

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 let randomNumber = Math.round(Math.random() * 10)
-
 let xAttempts = 1
 
-// Callback functions
+// Events
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', resetWithEnter)
+
+// Functions
 function handleTryClick(event) {
   event.preventDefault() // don't make the pattern, don't send the form
 
   const inputNumber = document.querySelector('#inputNumber')
   
   if (Number(inputNumber.value) === randomNumber) {
-    screen1.classList.toggle("hide")
-    screen2.classList.toggle("hide")
-    randomNumber = Math.round(Math.random() * 10)
+    toggleScreen()
+    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas!`
+  } else if (inputNumber.value == "" || Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10) {
+    screen1.querySelector("p").innerText = `Valor inválido! Informe um número entre 0 e 10`
+    screen1.querySelector("p").classList.add('red')
+    inputNumber.value = ""
+  } else {
+    inputNumber.value = ""
+    xAttempts ++
+    screen1.querySelector("p").innerText = `Adivinhe um número entre 0 e 10`
+    screen1.querySelector("p").classList.remove('red')
   }
-  
-  document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
-  
-  inputNumber.value = ""
-  xAttempts ++
 }
 
+function handleResetClick() {
+  toggleScreen()
+  xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10)
+  inputNumber.value = ""
+}
 
-// Events
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-  // window.location.reload()
+function toggleScreen() {
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
-  xAttempts = 1
-})
+}
+
+function resetWithEnter(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+}
